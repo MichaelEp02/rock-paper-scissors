907f8b3fccd9b671401a848bf21d99106b13ce2a
lolpng <michael.epstein02@gmail.com>
2023-08-22 07:05:05 +0300

    added playerSelection function

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function getComputerChoise(){
-    let choise = Math.floor(3*Math.random()); //getting a random number from 0 to 2
-    return choise;
+    let computerChoise = Math.floor(3*Math.random()); //getting a random number from 0 to 2
+    return computerChoise;
+}
+
+function getPlayerSelection(){
+    let playerSelection = prompt('eneter your selection (rock, paper, scissors):') ;
+    let flag = true;
+    playerSelection = playerSelection.toLowerCase()
+    while(flag)
+    switch (playerSelection){
+        case 'rock':
+            playerSelection = 0;
+            flag = false;
+            break;
+        case 'paper':
+            playerSelection = 1;
+            flag = false;
+            break;
+        case 'scissors':
+            playerSelection = 2;
+            flag = false;
+            break;
+        default:
+            playerSelection = prompt('check spelling and eneter again(rock, paper, scissors):');
+            break;
+    }
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection){
+
 }
 
 function printChoise(choise){
